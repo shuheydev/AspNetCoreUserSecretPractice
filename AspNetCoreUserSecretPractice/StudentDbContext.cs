b09ad26bf6921cd5b83add2132026bd33bf5da6a
Shuhey <shuhey.dev@gmail.com>
2020-04-05 16:31:03 +0900

    Add project files.

---
+++ AspNetCoreUserSecretPractice/StudentDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static AspNetCoreUserSecretPractice.Models;
+
+namespace AspNetCoreUserSecretPractice
+{
+    public class StudentDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
